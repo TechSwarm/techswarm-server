3f331f996620b17af2d19ec5cf41dd5b7cf39278
m4tx <m4tx@m4tx.pl>
2015-04-09 23:54:45 +0200

    Add simple install script

--- install.py
+++ install.py
@@ -1,1 +1,8 @@
-__author__ = 'm4tx'
+import sys
+import tsserver
+
+print("Creating database...", file=sys.stderr)
+print("Database URL: %s" % tsserver.db.engine.url, file=sys.stderr)
+tsserver.db.create_all()
+
+print("Everything is done!", file=sys.stderr)
