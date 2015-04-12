6c2deab13e5b608fe29d18ddb27c63945d883530
m4tx <m4tx@m4tx.pl>
2015-04-12 17:15:11 +0200

    Update install.py adding support for uploads
    Also, clean the file up

--- install.py
+++ install.py
@@ -1,8 +1,52 @@
+import os
 import sys
 import tsserver
+from tsserver import configutils
 
-print("Creating database...", file=sys.stderr)
-print("Database URL: %s" % tsserver.db.engine.url, file=sys.stderr)
+
+def msg(s, end=''):
+    if end == '':
+        s += ' '
+    print(s, file=sys.stderr, end=end)
+
+
+def msg_line(s):
+    msg(s, os.linesep)
+
+
+#
+# Uploads
+#
+def save_test():
+    with open(os.path.join(upload_folder, test_filename), 'w') as f:
+        f.write('test')
+
+
+msg("Checking if files can be saved in upload directory...")
+upload_folder = configutils.get_upload_dir()
+test_filename = 'install-script-test-file.txt'
+
+try:
+    save_test()
+except FileNotFoundError:
+    msg("Directory does not exist, creating it...")
+    os.makedirs(upload_folder, 0o744)
+    save_test()
+except PermissionError:
+    msg("Directory is non-writable, trying to chmod it...")
+    os.chmod(upload_folder, 0o744)
+    save_test()
+os.remove(os.path.join(upload_folder, test_filename))
+msg_line("ok")
+
+#
+# Database
+#
+msg("Creating database...")
+msg_line("Database URL: %s" % tsserver.db.engine.url)
 tsserver.db.create_all()
 
-print("Everything is done!", file=sys.stderr)
+#
+# Finish
+#
+msg_line("Everything is done!")
