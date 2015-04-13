ff2d0c4f754b68d25a6695927073022fb3647b67
m4tx <m4tx@m4tx.pl>
2015-04-13 16:34:35 +0200

    Remove 404 handling code
    Now the server uses 404 handling from Flask-RESTful

--- tsserver/__init__.py
+++ tsserver/__init__.py
@@ -6,7 +6,10 @@ from flask.ext.restful import Api
 app = Flask(__name__, static_folder='../files')
 app.config.from_object('config')
 db = SQLAlchemy(app)
-api = Api(app)
+api = Api(app, catch_all_404s=True)
 
-from tsserver import errorhandlers
+# Otherwise we're going to receive messages like
+# "You have requested this URI [/panorama] but did you mean /panorama ?"
+app.config['ERROR_404_HELP'] = False
+
 from tsserver import actions
--- tsserver/features/errorhandlers.feature
+++ tsserver/features/errorhandlers.feature
@@ -3,4 +3,4 @@ Feature: Error handlers
   Scenario: 404 handler
     When I request /lolwtfrandom1337
     Then 404 status code should be returned
-      And "message" key in JSON data should be equal to "Not found"
+      And "message" key in JSON data should be equal to "Not Found"
