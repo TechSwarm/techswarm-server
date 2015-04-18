0d7e0cf161713d274c24ad1f15d68e39226b4700
m4tx <m4tx@m4tx.pl>
2015-04-19 00:59:11 +0200

    Fix latitude and longitude ValueError messages

--- tsserver/inputtypes.py
+++ tsserver/inputtypes.py
@@ -6,12 +6,12 @@ timestamp = dtutils.datetime_from_str
 def latitude(value):
     value = float(value)
     if value < -90 or value > 90:
-        raise ValueError("Latitude is not in range [-90, 90]")
+        raise ValueError("Latitude is not within range [-90, 90]")
     return value
 
 
 def longitude(value):
     value = float(value)
     if value < -180 or value > 180:
-        raise ValueError("Latitude is not in range [-180, 180]")
+        raise ValueError("Longitude is not within range [-180, 180]")
     return value
