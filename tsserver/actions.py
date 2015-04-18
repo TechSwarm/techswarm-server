c15e80fc6f6c3bafc8221ed03e2eb209c6ddb330
m4tx <m4tx@m4tx.pl>
2015-04-18 12:15:58 +0200

    Clean up actions.py

--- tsserver/actions.py
+++ tsserver/actions.py
@@ -6,9 +6,14 @@ from tsserver.genericapi.models import Telemetry, Status
 
 
 bulk = Bulk(api)
-bulk.add_resource(CollectionGenericAPI.create(Telemetry), '/telemetry')
+
+# Status
 bulk.add_resource(CollectionGenericAPI.create(Status), '/status')
 api.add_resource(LatestElementGenericAPI.create(Status), '/status/current')
 
+# Photos
 api.add_resource(Photos, '/photos')
 api.add_resource(Photos.Panorama, '/panorama')
+
+# Other
+bulk.add_resource(CollectionGenericAPI.create(Telemetry), '/telemetry')
